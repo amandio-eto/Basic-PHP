bece9050d10344219d4200fc4c13526348d5e21b
amandio.eto <JoseAmandiodeAlmeida021099@gmail.com>
2022-12-09 11:06:17 +0900

    update function_return_value

--- function.php
+++ function.php
@@ -8,8 +8,15 @@ function name(){
 
 function hello($name){
 
-    echo "My Name Is $name";
+    echo "My Name Is $name".PHP_EOL;
 }
 
 echo name().PHP_EOL;
 echo hello("Jose Amandio de Almeida");
+function test( int $age)
+{
+    echo "My Name Is Age $age".PHP_EOL;
+    
+}
+
+echo test(23);
---
+++ function_return_value.php
@@ -0,0 +1,12 @@
+<?php
+
+
+function sum(int $x , int $y){
+
+    $total = $x * $y;
+    return $total;
+
+    
+}
+
+echo "Your Result is"." ".sum(10,9);
