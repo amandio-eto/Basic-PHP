fe4436331452bb86c49479b5e021c92beefc7760
amandio.eto <JoseAmandiodeAlmeida021099@gmail.com>
2022-12-09 11:51:18 +0900

    add string function, is_function

---
+++ is_function.php
@@ -0,0 +1,8 @@
+<?php
+$data = "Jose Amandio de Almeida";
+
+var_dump(is_string($data)).PHP_EOL;
+var_dump(is_bool($data)).PHP_EOL;
+var_dump(is_int($data)).PHP_EOL;
+var_dump(is_array($data)).PHP_EOL;
+var_dump(is_float($data)).PHP_EOL;
---
+++ string_function.php
@@ -0,0 +1,7 @@
+<?php
+
+var_dump(explode(" ","PHP DOCKER JENKINS JQUERY JAVASCRIPT"));
+var_dump(strtolower("PHP DOCKER JENKINS JQUERY JAVASCRIPT"));
+var_dump(strtoupper("php docker jenkins jquery javascript"));
+var_dump(trim("                 PHP                  "));
+var_dump(substr("Jose Amandio de Almeida",0,4));
